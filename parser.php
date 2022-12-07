34e1899ccc22fa4c8f29a9633a21b8bf94ebc940
Damian Wasiak <dwasiak@serif.net>
2022-12-07 19:21:48 +0100

    Pierwsza wersja (nieukoczona) parsera

---
+++ parser.php
@@ -0,0 +1,36 @@
+<?php
+//include_once ("wo_for_parse.html");
+
+function r_2($val){
+	echo '<pre>' ;print_r ($val); echo '</pre>';
+}
+
+function getTable(){
+	$tableFile = file_get_contents('wo_for_parse.html');
+	$DOM = new DOMDocument;
+	$DOM->preserveWhiteSpace = false;
+	$DOM->formatOutput = true;
+	$DOM->loadHTML($tableFile);
+	$tableHeaders = $DOM->getElementsByTagName('div');
+	$tableData = $DOM->getElementsByTagName('h3');
+	
+	
+	/*print_r ($tableHeaders);
+	echo '<br>';
+	print_r ($tableData);
+	echo '<br><br>';*/
+	
+	foreach ($tableData as $Data)
+	{ 
+		echo '<pre>'; 
+		echo trim($Data->nodeValue); 
+		echo '</pre>'; 
+	}
+	
+	foreach ($tableData as $Data)
+	{ echo '<pre>'; print_r($Data); echo '</pre>'; }
+}
+
+getTable();
+?>
+
