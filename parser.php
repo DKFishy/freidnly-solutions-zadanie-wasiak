9b54806575284022b83333b49bae4399581e7acb
Damian Wasiak <dwasiak@serif.net>
2022-12-09 23:22:56 +0100

    Dodano podział ulicy, zamianę typu danych, oraz ograniczono zakres danych pobieranych z tabeli. Do dodania: export do csv.

--- parser.php
+++ parser.php
@@ -17,8 +17,7 @@ function getTable(){
 	$dataValue = '';
 	$dataArray = array();
 	foreach ($tableData as $Data)
-	{ 
-		echo '<pre>'; 
+	{
 		$dataValue = $Data->nodeValue; 
 		
 		$dataValue = preg_replace('/[[:blank:]]{3,}/', '', $dataValue);
@@ -28,24 +27,39 @@ function getTable(){
 		$dataValue = preg_replace('/( )$/', '', $dataValue);		
 
 		if ($dataIndex == '16'){
-			$splitAddArr = explode(' ', $dataValue); //zamiana string do array
+			$strToArr = explode(' ', $dataValue); //zamiana wartości komórki tabeli z string do array
+			$splitAddress = array_slice($strToArr, 0, 3);
+			$splitAddress = implode(' ', $splitAddress);
+			$splitAddress = array($splitAddress);
+			array_push($splitAddress, $strToArr[4], $strToArr[5]);
 			foreach ($splitAddress as $SA){
 				$dataArray[$dataIndex] = $SA;
 				$dataIndex++;
 			}
-			
+		} elseif ($dataIndex == 3){
+			$dataValue = strtotime($dataValue);
+			$dataValue = date('Y-m-d H:i', $dataValue);
+			$dataArray[$dataIndex] = $dataValue;
+			$dataIndex++;
+		} elseif ($dataIndex == 5){
+			$dataValue = preg_replace('/\$|\,/', '', $dataValue);
+			$dataArray[$dataIndex] = floatval($dataValue);
+			$dataIndex++;
+		} elseif ($dataIndex == 19){
+			$dataValue = preg_replace('/\D/', '', $dataValue);
+			$dataArray[$dataIndex] = floatval($dataValue);
+			$dataIndex++;
 		} else {
 			$dataArray[$dataIndex] = $dataValue;
 			$dataIndex++;
 		}
-		
-		
-		//echo $dataIndex . ' ' . $dataValue;
-		echo '</pre>'; 
-		/*$dataArray[$dataIndex] = $dataValue;
-		$dataIndex++;*/
 	}
-	echo '<pre>'; print_r($dataArray); echo '</pre>';
+	$finalArr = array();
+	$indexArr = array(0, 2, 3, 5, 10, 11, 14, 16, 17, 18, 19);
+	foreach ($indexArr as $index){
+		array_push($finalArr, $dataArray[$index]);
+	}
+	echo '<pre>'; print_r($finalArr); echo '</pre>';
 	$unset($dataIndex);
 	
 	/*foreach ($tableHeaders as $Header)
